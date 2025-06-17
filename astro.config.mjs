055d02dbbb8143f1fa9ec3160baac0fc4f601141
therealkiri <beol0127@gmail.com>
2025-06-16 23:34:41 -0400

    fix: refixing the fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://frc6423.org",
+  site: 'https://wmironpatriots.github.io',
 });
